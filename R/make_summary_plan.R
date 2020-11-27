213868c2579a5650a0511d9616cef29b6f2a7d33
nhowlett <nhowlett87@gmail.com>
2020-11-27 16:43:18 +0000

    updated the plan logic

--- R/make_summary_plan.R
+++ R/make_summary_plan.R
@@ -2,53 +2,58 @@ make_summary_plan <- function(data,
                               summary_formula = list(numeric = is.numeric ~ sum_mean,
                                                      factor = is.factor ~ sum_fact),
                               by = NULL){
-  # grab predicate functions and assoc summary functions
-  sum_fmla <- list(numeric = is.numeric ~ sum_mean + sum_iqr,
-                   factor = is.factor ~ sum_fact)
-
-  sum_fun <- purrr::map(sum_fmla, ~purrr::pluck(.x, 3) %>%
-                        as.list() %>%
-                        purrr::discard(~deparse(.x) == "+"))
-
-  by <- rlang::enquo(by)
-  # if by (unquoted) is NULL, create blank quosure
-  if(is.null(rlang::quo_get_expr(by))){
+  browser()
+  # input validation
+  if(!all(map_lgl(summary_formula, check_fmla_operators))) stop("Your summary formulae contain operators other than `~` & `|`, which is currently not supported")
 
 
-    plan <-  pred_summary %>%
-      # select variables of each type
-      purrr::imap(~dplyr::select(data, where(.x))) %>%
-      # drop any empty datasets
-      purrr::discard(~ncol(.x) == 0) %>%
-      # add grouping variable
-      purrr::map(~tidyr::pivot_longer(data = .x,
-                                      cols = dplyr::everything(),
-                                      names_to = "variable") %>%
-                   dplyr::group_by(variable) %>%
-                   nest() %>%
-                   dplyr::mutate(statistic = statistic))
+  summary_plan <- purrr::map(summary_formula,
+                             ~list(predicate = get_fmla_response(.x),
+                                   summary = get_fmla_predictors(.x)))
+  
+  summary_plan <- summary_plan %>%
+                  map(~c(data = pluck(.x, 1) %>% map(~select(data, where(get(.x)))),
+                          fn = pluck(.x, 2))) %>%
+                  discard( ~ncol(.x$data) == 0)
 
-
-  } else {
-
-    # take grouping variable(s)
-    df_grps <- data %>%
-      select(!!by)
-
-
-    plan <-  class_tests %>%
-      # select variables of each type (minus grouping var)
-      purrr::imap(~dplyr::select(data, where(.x), -!!by)) %>%
-      # drop any empty datasets
-      purrr::discard(~ncol(.x) == 0) %>%
-      # add grouping variable
-      purrr::map(~bind_cols(.x, df_grps) %>%
-                   tidyr::pivot_longer(-!!by, names_to = "variable") %>%
-                   dplyr::group_by(!!by, variable) %>%
-                   nest() %>%
-                   dplyr::mutate(statistic = statistic))
-
-  }
+  
+  # by <- rlang::enquo(by)
+  # # if by (unquoted) is NULL, create blank quosure
+  # if(is.null(rlang::quo_get_expr(by))){
+  #   summary_plan <-  summary_plan %>%
+  #     # select variables of each type
+  #     purrr::pmap() %>%
+  #     # drop any empty datasets
+  #     purrr::discard(~ncol(.x) == 0) %>%
+  #     # add grouping variable
+  #     purrr::map(~tidyr::pivot_longer(data = .x,
+  #                                     cols = dplyr::everything(),
+  #                                     names_to = "variable") %>%
+  #                  dplyr::group_by(variable) %>%
+  #                  nest() %>%
+  #                  dplyr::mutate(statistic = statistic))
+  # 
+  # 
+  # } else {
+  # 
+  #   # take grouping variable(s)
+  #   df_grps <- data %>%
+  #     select(!!by)
+  # 
+  # 
+  #   plan <-  class_tests %>%
+  #     # select variables of each type (minus grouping var)
+  #     purrr::imap(~dplyr::select(data, where(.x), -!!by)) %>%
+  #     # drop any empty datasets
+  #     purrr::discard(~ncol(.x) == 0) %>%
+  #     # add grouping variable
+  #     purrr::map(~bind_cols(.x, df_grps) %>%
+  #                  tidyr::pivot_longer(-!!by, names_to = "variable") %>%
+  #                  dplyr::group_by(!!by, variable) %>%
+  #                  nest() %>%
+  #                  dplyr::mutate(statistic = statistic))
+  # 
+  # }
 
 
   plan
--- R/utils.R
+++ R/utils.R
@@ -1,17 +1,30 @@
+check_fmla_operators <- function(fmla) {
+  # this function checks if the operators in fmla are in c("~", "+"), otherwise return FALSE
+  operators <- intersect(names(.Options$operators), all.names(fmla))
+  all(operators %in% c("+", "~")) 
+} 
+
 get_fmla_response <- function(fmla) {
- if(!is_two_sided(fmla)) stop("Input formula is not two-sided")
- elements <- attr(terms(fmla), which = "variables") 
- elements[[2]]
+ if(is_two_sided(fmla)) stop("Input formula is not one-sided")
+  # get all vars
+  vars <- attr(terms(formula(fmla)), which = "variables")
+  # get response
+  res <- as.character(vars[[2]]) # The response variable name
+  res
 }
 
+# get_fmla_predictors <- function(fmla) {
+#  if(is_two_sided(fmla)){
+#    all.vars(fmla[-2])
+#  } else {
+#    labels(terms(fmla))
+#  }
+# }
+
 get_fmla_predictors <- function(fmla) {
- if(is_two_sided(fmla)){
-   all.vars(fmla[-2])
- } else {
-   elements <- attr(terms(fmla), which = "variables") 
-   elements %>% map(as.symbol)
- }
+ labels(terms(fmla))
 }
+
 
 is_two_sided <- function(fmla) {
   is.name(fmla[[1]]) && length(fmla) == 3 &&
