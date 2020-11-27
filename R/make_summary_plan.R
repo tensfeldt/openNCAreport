1fea42ffdcf53deee99640b202ef2e3d22db3388
nhowlett <nhowlett87@gmail.com>
2020-11-27 17:35:02 +0000

    worked on more of the plan generation logical
    need to fix the conditional behaviour on grouping/not grouping

--- R/make_summary_plan.R
+++ R/make_summary_plan.R
@@ -4,17 +4,44 @@ make_summary_plan <- function(data,
                               by = NULL){
   browser()
   # input validation
-  if(!all(map_lgl(summary_formula, check_fmla_operators))) stop("Your summary formulae contain operators other than `~` & `|`, which is currently not supported")
+  if(!all(purrr::map_lgl(summary_formula, check_fmla_operators))) stop("Your summary formulae contain operators other than `~` & `|`, which is currently not supported")
 
+  # sort out grouping vars
+  by <- rlang::enquo(by)
+  # if by (unquoted) is NULL, create blank quosure
+  # take grouping variable(s)
+  df_grps <- data %>%
+             dplyr::select(!!by)
 
+  
+  # tidy plan
   summary_plan <- purrr::map(summary_formula,
                              ~list(predicate = get_fmla_response(.x),
                                    summary = get_fmla_predictors(.x)))
   
+  
+  
+  
   summary_plan <- summary_plan %>%
-                  map(~c(data = pluck(.x, 1) %>% map(~select(data, where(get(.x)))),
-                          fn = pluck(.x, 2))) %>%
-                  discard( ~ncol(.x$data) == 0)
+                  # from the plan, select the data based on predicate and store the asssoc summary fns
+                  purrr::map(~c(data = purrr::pluck(.x, 1) %>%
+                                        purrr::map(~dplyr::select(data, where(get(.x)))),
+                                fn = purrr::pluck(.x, 2))) %>%
+                  # drop entries with no data
+                  purrr::discard(~ncol(.x$data) == 0) %>%
+                  # TODO fix this conditional part
+                  # conditionally add grouping variables
+                  # purrr::map(purrr::modify_if(.p = !is.null(rlang::quo_get_expr(by)),
+                  #                             ~.x$data = cbind(df_grps, .x$data))) %>%
+                  # pivot_longer all the data
+                  purrr::map(~purrr::modify_at(.x, .at = "data",
+                                               ~tidyr::pivot_longer(data = .x,
+                                                                    cols = dplyr::everything(),
+                                                                    names_to = "variable") %>%
+                                               dplyr::group_by(variable) %>%
+                                               tidyr::nest()))
+                  
+                  
 
   
   # by <- rlang::enquo(by)
