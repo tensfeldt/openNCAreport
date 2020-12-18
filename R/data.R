dbef7208abe55b1f907d6f12c593b1a1e31a5971
niklz <niklz.q3@gmail.com>
2020-12-18 12:58:15 +0000

    updated docs

--- R/data.R
+++ R/data.R
@@ -5,7 +5,7 @@
 #' @format A list with 204 slots, one per parameter, each slow has a further 8 nested slots
 #'
 #' \describe{
-#'   \item{parameter label}{price, in US dollars}
+#'   \item{parameter label}{The paramter label for presentation}
 #'   \item{cdisc_label}{}
 #'   \item{regex}{A RegEx pattern that links vars in the params data to their slot here}
 #'   \item{valid_models}{}
